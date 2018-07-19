3e47836ce5de31f40faf27ba634ea1b04f93f255
TitouanT <titouan.teyssier@gmail.com>
2018-07-19 05:13:57 +0200

    begin the second assignement

---
+++ HA2_pizzaDelivery/index.js
@@ -0,0 +1,25 @@
+/*
+*
+* Main file of the pizza delivery service
+*
+*/
+
+
+//dependencies
+const https = require('https');
+const config = require('config');
+const debug = require('util').debuglog(__filename.split('/').splice(-1,1)[0].replace('.js', ''));
+
+const server = https;
+
+
+// declare the application
+const app = {};
+
+//
+app.init = () => {
+
+}
+
+
+app.init();
